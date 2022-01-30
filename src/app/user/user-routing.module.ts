6840835a5057c9fc41875dd4fdf9a3575aebe3fa
AsmitaC19 <88437259+AsmitaC19@users.noreply.github.com>
2022-01-30 12:27:46 +0530

    Create user-routing.module.ts

---
+++ src/app/user/user-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SpecialEventsComponent } from './special-events/special-events.component';
+import { AuthGuard } from '../auth.guard';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'special-events',
+    canActivate: [AuthGuard],
+    component: SpecialEventsComponent
+  }
+]
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UserRoutingModule 
+{ 
+}
