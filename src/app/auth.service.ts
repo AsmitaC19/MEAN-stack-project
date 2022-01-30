73a169f5d130e05c11110625ce16ee963bf6e58d
AsmitaC19 <88437259+AsmitaC19@users.noreply.github.com>
2022-01-30 10:30:41 +0530

    Update auth.service.ts

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,30 +1,61 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
-import { Router } from '@angular/router'
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthService 
 {
 
   private _loginUrl = "http://localhost:3000/api/login";
+  private _adminLoginUrl="http://localhost:3000/api/adminLogin";
 
   constructor(private http: HttpClient,
-              private _router: Router) { }
+              private _router: Router) 
+  { 
+  }
 
-  loginUser(user) {
+  loginUser(user):Observable<any> 
+  {
     return this.http.post<any>(this._loginUrl, user)
   }
 
-  logoutUser() {
-    localStorage.removeItem('token')
+  userLogout() 
+  {
+    localStorage.removeItem('token1')
     this._router.navigate(['/events'])
   }
 
-  getToken() {
-    return localStorage.getItem('token')
+  adminLogin(admin)
+  {
+  return this.http.post<any>(this._adminLoginUrl,admin)
   }
 
-  loggedIn() {
-    return !!localStorage.getItem('token')    
+  adminLogout()
+  {
+    localStorage.removeItem('token2')
+    this._router.navigate(['/admin'])
   }
+
+  getToken1() 
+  {
+    return localStorage.getItem('token1')
+  }
+
+  getToken2() 
+  {
+    return localStorage.getItem('token2')
+  }
+
+  userLoggedIn()
+  {
+    return !!localStorage.getItem('token1')
+  }
+
+  adminLoggedIn()
+  {
+    return !!localStorage.getItem('token2')
+  }
+
+
 }
