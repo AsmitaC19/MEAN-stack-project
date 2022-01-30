6f112d07f8bddb372563485a5c5958e5b6112124
AsmitaC19 <88437259+AsmitaC19@users.noreply.github.com>
2022-01-30 10:34:28 +0530

    Update and rename token-interceptor.service.ts to token-interceptor1.service.ts

--- src/app/token-interceptor1.service.ts
+++ src/app/token-interceptor1.service.ts
@@ -2,17 +2,21 @@ import { Injectable, Injector } from '@angular/core';
 import { HttpInterceptor } from '@angular/common/http'
 import { AuthService } from './auth.service';
 @Injectable()
-export class TokenInterceptorService implements HttpInterceptor {
-
-  constructor(private injector: Injector){}
-  intercept(req, next) {
+export class TokenInterceptor1Service implements HttpInterceptor 
+{
+  constructor(private injector: Injector)
+  {
+  }
+  intercept(req, next) 
+  {
     let authService = this.injector.get(AuthService)
     let tokenizedReq = req.clone(
       {
-        headers: req.headers.set('Authorization', 'bearer ' + authService.getToken())
+        headers: req.headers.set('Authorization', 'bearer ' + authService.getToken1())
       }
     )
     return next.handle(tokenizedReq)
+  
   }
 
 }
