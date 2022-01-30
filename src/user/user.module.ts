c77dd543a2d1f464e900bdcdf4a6ef6d1bf0939d
AsmitaC19 <88437259+AsmitaC19@users.noreply.github.com>
2022-01-30 12:20:33 +0530

    Create user.module.ts

---
+++ src/user/user.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { SpecialEventsComponent } from './special-events/special-events.component';
+import { FormsModule } from '@angular/forms';
+import { UserRoutingModule } from './user-routing.module';
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    SpecialEventsComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    UserRoutingModule
+  ]
+})
+export class UserModule 
+{ 
+  
+}
