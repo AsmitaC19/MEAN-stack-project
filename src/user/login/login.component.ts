8b6ebf6848faf959b3cca83b52dd38f0c2946c49
AsmitaC19 <88437259+AsmitaC19@users.noreply.github.com>
2022-01-30 10:49:03 +0530

    Create login.component.ts

---
+++ src/user/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../auth.service';
+
+@Component({
+  selector: 'user-login',
+  templateUrl: './login.component.html'
+})
+export class LoginComponent implements OnInit 
+{
+
+  loginUserData = 
+    {
+    email:'',
+    password:''
+    };
+
+  constructor(private _auth: AuthService,
+              private _router: Router) 
+  { 
+  }
+
+  ngOnInit() 
+  {
+  }
+
+  loginUser () 
+  {
+    this._auth.loginUser(this.loginUserData)
+    .subscribe(
+      res => 
+      {
+        localStorage.setItem('token1', res.token)
+        this._router.navigate(['/user/special-events'])
+      },
+      err => console.log(err)
+    ) 
+  }
+}
