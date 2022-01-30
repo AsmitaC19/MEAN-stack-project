c54f3899bf0a77327f87e5c938de19f30d876927
AsmitaC19 <88437259+AsmitaC19@users.noreply.github.com>
2022-01-30 10:26:08 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,8 @@ import { AuthService } from './auth.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent 
+{
   title = 'app';
   constructor(public _authService: AuthService){}
 }
