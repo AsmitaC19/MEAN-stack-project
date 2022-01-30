0a1523fa2ebbb6cb6cfb7f07c893727094e60742
AsmitaC19 <88437259+AsmitaC19@users.noreply.github.com>
2022-01-30 12:33:30 +0530

    Create admin-login.component.ts

---
+++ src/app/admin/admin-login/admin-login.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../auth.service';
+
+@Component({
+  selector: 'admin-admin-login',
+  templateUrl: './admin-login.component.html'
+})
+export class AdminLoginComponent implements OnInit 
+{
+
+  loginAdminData = {
+    email:'',
+    password:''
+    };
+
+  constructor(private _auth: AuthService,
+              private _router: Router) 
+  { 
+  }
+
+  ngOnInit() 
+  {
+  }
+
+  adminLogin () 
+  {
+    this._auth.adminLogin(this.loginAdminData)
+    .subscribe(
+      res => {
+        localStorage.setItem('token2', res.token)
+        this._router.navigate(['/admin/admin-special'])
+      },
+      err => console.log(err)
+    ) 
+  }
+
+}
